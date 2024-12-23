714ed2131721686866ebb850be1febd84aa661dc
dineshwpbot <herathmadusankadinesh@gmail.com>
2024-12-23 14:31:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'add your sesson id',
+    SESSION_ID: process.env.SESSION_ID || 'umIHHSQY#G8mu8YPbtiHpqh20jlLAG09NP-sRFaO_bvVWPZvshgY',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
