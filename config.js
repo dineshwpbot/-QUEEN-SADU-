0664d47af2380ed8b49c2ad91fb6fdad572b7d70
dineshwpbot <herathmadusankadinesh@gmail.com>
2024-12-23 16:30:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'QUEENELISA;;;==ANjVTWF91N69ESuNURV50SvRzYoRFTTZ3M4hXLQ9WZG1kNkVFRxMUdtg1TjI2bBNVQORWRpwd=46410088749',
+    SESSION_ID: process.env.SESSION_ID || 'IbE1HRKT#yJc9Ktd1vdzi1uno0EisEgu8xrHJs0Kn4uOWNtWyvYw',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
