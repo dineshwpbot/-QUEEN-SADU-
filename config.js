0c72444821a348131ece1f5d663f01dc46b531a1
dineshwpbot <herathmadusankadinesh@gmail.com>
2024-12-23 16:16:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'ANjVTWF91N69ESuNURV50SvRzYoRFTTZ3M4hXLQ9WZG1kNkVFRxMUdtg1TjI2bBNVQORWRpwd=46410088749',
+    SESSION_ID: process.env.SESSION_ID || 'QUEENELISA;;;==ANjVTWF91N69ESuNURV50SvRzYoRFTTZ3M4hXLQ9WZG1kNkVFRxMUdtg1TjI2bBNVQORWRpwd=46410088749',
     MONGODB: process.env.MONGODB || 'mongodb+srv://udavin56:1234@cluster0.urhma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 };
